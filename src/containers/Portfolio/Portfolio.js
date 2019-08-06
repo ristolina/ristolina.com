d353cf51de498cf2807bba89883caac6b0097bb6
Kristoffer <kb@ristolina.com>
2019-08-06 16:30:49 +0200

    Changed ReadMe

--- src/containers/Portfolio/Portfolio.js
+++ src/containers/Portfolio/Portfolio.js
@@ -8,7 +8,8 @@ const Portfolio = () => {
   const style ={backgroundImage: 'url(http://mrmrs.github.io/images/0006.jpg)'}
   return (
     <div>
-      <section className="center mw5">
+      <section className="center
+        ">
         <h2 className="fw3 f3 white-80 mt3 mb4">portfolio</h2>
         <h3 className="fw3 f3 white-80 mt3 mb4">Portfolio is in the works</h3>
       </section>
