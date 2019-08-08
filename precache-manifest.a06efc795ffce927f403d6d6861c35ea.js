195e0c862d20307ad344d2744e03221ddfa20531
Kristoffer <kb@ristolina.com>
2019-08-08 17:41:13 +0200

    Updates

---
+++ precache-manifest.a06efc795ffce927f403d6d6861c35ea.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f988ee03264b0cec9f4d771ad13f533e",
+    "url": "ristolina.github.io/index.html"
+  },
+  {
+    "revision": "83e9c6de46a07042768f",
+    "url": "ristolina.github.io/static/css/2.9776ef19.chunk.css"
+  },
+  {
+    "revision": "c89b1ad38ae2dad64c5c",
+    "url": "ristolina.github.io/static/css/main.633d3542.chunk.css"
+  },
+  {
+    "revision": "83e9c6de46a07042768f",
+    "url": "ristolina.github.io/static/js/2.db66e26c.chunk.js"
+  },
+  {
+    "revision": "c89b1ad38ae2dad64c5c",
+    "url": "ristolina.github.io/static/js/main.c3e6d2d9.chunk.js"
+  },
+  {
+    "revision": "d67f2e54b751e798776a",
+    "url": "ristolina.github.io/static/js/runtime~main.7d44ae85.js"
+  },
+  {
+    "revision": "42fd2d744343d3a64ce296b0fcc475e3",
+    "url": "ristolina.github.io/static/media/background.42fd2d74.jpg"
+  },
+  {
+    "revision": "8c7c6f3d57baa683ec3b9ecea428f2e0",
+    "url": "ristolina.github.io/static/media/brackets.8c7c6f3d.svg"
+  },
+  {
+    "revision": "5291b74f9407cae38a2f48f873b492de",
+    "url": "ristolina.github.io/static/media/code.5291b74f.svg"
+  },
+  {
+    "revision": "4de2eb4d518c8ce43d9425e5d3260691",
+    "url": "ristolina.github.io/static/media/ristolina-logo_mono-light.4de2eb4d.svg"
+  },
+  {
+    "revision": "8baa2610a040797b003f166bbc323cf9",
+    "url": "ristolina.github.io/static/media/terminal.8baa2610.svg"
+  }
+]);
